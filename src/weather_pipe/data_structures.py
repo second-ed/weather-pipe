be5c45188397094d6d4a47440c0d800637bd9d68
ed cuss <edcussmusic@gmail.com>
2025-03-20 21:59:16 +0000

    feat: initial functions bronze layer

--- src/weather_pipe/data_structures.py
+++ src/weather_pipe/data_structures.py
@@ -1,4 +1,5 @@
 import attrs
+import polars as pl
 
 
 @attrs.define
@@ -6,3 +7,30 @@ class ApiConfig:
     api_key: str = attrs.field(repr=False)
     location: str = attrs.field()
     request_type: str = attrs.field()
+
+
+@attrs.define
+class RawTables:
+    fact_table: pl.DataFrame = attrs.field(repr=False)
+    dim_tables: dict | None = attrs.field(default=None, repr=False)
+    cols: list | None = attrs.field(default=None)
+
+
+@attrs.define
+class UnnestedTables(RawTables):
+    pass
+
+
+@attrs.define
+class CleanedTables(RawTables):
+    pass
+
+
+@attrs.define
+class NormalisedTables(RawTables):
+    pass
+
+
+@attrs.define
+class EncodedTables(RawTables):
+    pass
--- src/weather_pipe/transform.py
+++ src/weather_pipe/transform.py
@@ -2,9 +2,17 @@ import uuid
 from copy import deepcopy
 from datetime import datetime
 
+import attrs
 import polars as pl
-from returns.result import Failure, Result, Success
+from returns.result import Failure, Result, Success, safe
 
+from weather_pipe.data_structures import (
+    CleanedTables,
+    EncodedTables,
+    NormalisedTables,
+    RawTables,
+    UnnestedTables,
+)
 from weather_pipe.utils import log_call
 
 
@@ -40,3 +48,56 @@ def add_ingestion_columns(
         return Success(df)
     except Exception as e:
         return Failure({"err": str(e)})
+
+
+@log_call
+@safe
+def unnest_struct_cols(raw_tables: RawTables) -> UnnestedTables:
+    unnested_tables = UnnestedTables(*attrs.astuple(raw_tables))
+    unnested_tables.fact_table = unnested_tables.fact_table.unnest(
+        [
+            col
+            for col in unnested_tables.fact_table.columns
+            if unnested_tables.fact_table.schema[col] == pl.Struct
+        ]
+    )
+    return unnested_tables
+
+
+@log_call
+@safe
+def clean_text_cols(unnested_tables: UnnestedTables) -> CleanedTables:
+    cleaned_tables = CleanedTables(*attrs.astuple(unnested_tables))
+    cleaned_tables.fact_table = cleaned_tables.fact_table.with_columns(
+        [
+            pl.col(col).str.strip_chars().str.to_lowercase()
+            for col in cleaned_tables.fact_table.columns
+            if cleaned_tables.fact_table.schema[col] == pl.Utf8
+        ]
+    )
+    return cleaned_tables
+
+
+@log_call
+@safe
+def normalise_table(cleaned_tables: CleanedTables) -> NormalisedTables:
+    norm_tables = NormalisedTables(*attrs.astuple(cleaned_tables))
+    norm_tables.dim_tables = {}
+    for col in norm_tables.cols:
+        norm_table = norm_tables.fact_table[col].unique().to_frame()
+        norm_table = norm_table.with_columns(
+            pl.arange(0, norm_table.height).alias(f"{col}_id")
+        )
+        norm_tables.dim_tables[col] = norm_table.select(f"{col}_id", col)
+    return norm_tables
+
+
+@log_call
+@safe
+def replace_col_with_id(norm_tables: NormalisedTables) -> EncodedTables:
+    encoded_tables = EncodedTables(*attrs.astuple(norm_tables))
+    for col, dim in encoded_tables.dim_tables.items():
+        encoded_tables.fact_table = encoded_tables.fact_table.join(
+            dim, on=col, how="left"
+        ).drop(col)
+    return encoded_tables
